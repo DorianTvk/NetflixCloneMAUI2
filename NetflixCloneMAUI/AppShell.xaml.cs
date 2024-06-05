9721e67fd8a0533ca29b33e66ea725aa520ee9fe
Opilane TTHK <opilane@tthk.ee>
2024-06-05 11:06:26 +0300

    SS

---
+++ NetflixCloneMAUI/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using NetflixCloneMAUI.Pages;
+namespace NetflixCloneMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(CategoriesPage), typeof(CategoriesPage));
+        }
+    }
+}
